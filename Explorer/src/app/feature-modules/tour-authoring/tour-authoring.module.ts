6eee24fb0e50c79cc0aa4ae03aa7297cfc8d8ead
Nikolina Ivošević <ivosevicnina1@gmail.com>
2023-10-24 16:34:50 +0200

    feat/tour-form and tour component

--- Explorer/src/app/feature-modules/tour-authoring/tour-authoring.module.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tour-authoring.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TourFormComponent } from './tour-form/tour-form.component';
+import { TourComponent } from './tour/tour.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TourFormComponent,
+    TourComponent
+  ],
   imports: [
     CommonModule
   ]
