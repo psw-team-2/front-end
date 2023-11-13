693fa2abeea397925d3118682485939e05eba18b
Milos Diklic <milosdiklic25@gmail.com>
2023-11-14 00:29:05 +0100

    feat: tour overview added

--- Explorer/src/app/feature-modules/tour-authoring/tour-overview/tour-overview.component.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tour-overview/tour-overview.component.ts
@@ -1,23 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Checkpoint } from '../model/checkpoint.model';
 import { Tour } from '../model/tour.model';
 import { TourAuthoringService } from '../tour-authoring.service';
 import { forkJoin } from 'rxjs';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { TourReview } from '../../marketplace/model/tour-review.model';
+import { MarketplaceService } from '../../marketplace/marketplace.service';
 
 @Component({
   selector: 'xp-tour-overview',
   templateUrl: './tour-overview.component.html',
-  styleUrls: ['./tour-overview.component.css']
+  styleUrls: ['./tour-overview.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('300ms', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class TourOverviewComponent {
-
   tour: Tour;
   tourId: number | null;
   checkpoints: Checkpoint[] = [];
   canRender: boolean = false;
+  images: string[] = [];
+  currentIndex: number = 0;
+  reviews: TourReview[] = [];
 
-  constructor(private tourService: TourAuthoringService, private route: ActivatedRoute) { }
+  constructor(private tourService: TourAuthoringService, private route: ActivatedRoute, private marketplaceService: MarketplaceService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -30,8 +46,21 @@ export class TourOverviewComponent {
             this.fetchCheckpointsForTour(this.tourId);
           }
         });
+        this.fetchTourReviews();
       } else {
         // Handle the case where id is null
+      }
+    });
+  }
+
+  difficultyStars(difficulty: number): number[] {
+    return Array(difficulty).fill(1);
+  }
+
+  fetchTourReviews(): void {
+    this.marketplaceService.getTourReview().subscribe({
+      next: (result) => {
+        this.reviews = result.results;
       }
     });
   }
@@ -54,6 +83,7 @@ export class TourOverviewComponent {
           this.checkpoints = checkpoints;
           this.canRender = true; // Set canRender to true when all checkpoints are fetched.
           console.log(new Date());
+          this.images = this.checkpoints.map(checkpoint => checkpoint.image);
         },
         error: (error) => {
           // Handle errors if necessary
@@ -62,5 +92,17 @@ export class TourOverviewComponent {
     } else {
       this.canRender = true; // If there are no checkpoint IDs, set canRender to true immediately.
     }
+  }
+
+  getCurrentImage(): string {
+    return this.images[this.currentIndex];
+  }
+
+  prevImage() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
   }
 }
