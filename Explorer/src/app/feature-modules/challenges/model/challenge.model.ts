651d02d68ee2a0353c423a7e13cb27e6aaab689a
Milos Diklic <milosdiklic25@gmail.com>
2023-11-22 17:01:39 +0100

    feat: challenges added to map search

---
+++ Explorer/src/app/feature-modules/challenges/model/challenge.model.ts
@@ -0,0 +1,10 @@
+export interface Challenge {
+    id?: number;
+    name : string;
+    description: string;
+    latitude : number;
+    longitude : number;
+    xp : number;
+    status : number;
+    type : number;
+}
