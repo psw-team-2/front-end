d12d7f91c32b04c3eef0371c056be23058a9e000
Nina <nina.batranovic3@gmail.com>
2023-12-01 19:04:26 +0100

    bug fixed

--- Explorer/src/app/shared/model/paged-results.model.ts
+++ Explorer/src/app/shared/model/paged-results.model.ts
@@ -1,5 +1,4 @@
 export interface PagedResults<T>{
-    items: import("d:/PSW projekat/front-end/Explorer/src/app/feature-modules/tour-authoring/model/tour.model").Tour[];
     results: T[];
     totalCount: number;
 }
