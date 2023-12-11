eb67537321388a330969ec6733e0ca8d7d4c6905
ayadjo <andjela1108@gmail.com>
2023-12-11 21:32:14 +0100

    style: Improved the appearance of the rating display

--- Explorer/src/app/app.module.ts
+++ Explorer/src/app/app.module.ts
@@ -38,7 +38,7 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 @NgModule({
   declarations: [
     AppComponent,
-    SaleComponent,
+    SaleComponent
   ],
   imports: [
     BrowserModule,
