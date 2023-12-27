739f15025e7a955aa9317c4185cf3a9949c498af
kirdatatjana <kirda.tatjana@gmail.com>
2023-12-27 12:47:11 +0100

    added greetings

--- Explorer/src/app/feature-modules/layout/start-your-journey/start-your-journey.component.ts
+++ Explorer/src/app/feature-modules/layout/start-your-journey/start-your-journey.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Profile } from '../../administration/model/profile.model';
+import { AdministrationService } from '../../administration/administration.service';
 
 @Component({
   selector: 'xp-start-your-journey',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./start-your-journey.component.css']
 })
 export class StartYourJourneyComponent {
+  profile: Profile[] = [];
+  currentProfile: string
+ 
+  
+  constructor(private service: AdministrationService , private router: Router) { }
 
+  ngOnInit(): void {
+    
+    this.getByUserId();
+
+  }
+  getByUserId(): void {
+    this.service.getByUserId().subscribe({
+      next: (result: Profile) => {
+        console.log('Result from API:', result);
+        this.profile = [result]; // Wrap the result in an array, as it's a single Profile object
+        console.log('Profile data in component:', this.profile);
+        this.currentProfile= this.profile[0].firstName;
+        this.currentProfile = this.profile[0].firstName.toUpperCase();
+
+      },
+      error: (err: any) => {
+        console.log(err);
+      }
+    });
+  }
 }
