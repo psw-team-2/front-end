715f6a57edb9550d22680814f9dec78fc51e3313
Nina <nina.batranovic3@gmail.com>
2023-12-28 15:49:13 +0100

    bug fixed, css modified

--- Explorer/src/app/feature-modules/tour-execution/active-tour/active-tour.component.ts
+++ Explorer/src/app/feature-modules/tour-execution/active-tour/active-tour.component.ts
@@ -126,7 +126,17 @@ export class ActiveTourComponent implements OnInit {
     return true;
   }
   
+  allStorage() {
+    var values = [],
+      keys = Object.keys(localStorage),
+      i = keys.length;
 
+    while (i--) {
+      values.push(localStorage.getItem(keys[i]));
+    }
+
+    return values;
+  }
 
 
   ispisEncounters() {
